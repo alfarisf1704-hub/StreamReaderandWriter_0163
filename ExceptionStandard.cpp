f47e4cf6b958ca6c4734b0840f28fdafff3b175c
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-06-01 14:48:11 +0700

    melengkapi kommant

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -7,8 +7,10 @@ int main()
 {
     cout << "Awal Program" << endl; // penanda 1
     try
-    {
+    { // deklarasi array index = 3
         array<int, 3> data = {1, 2, 3};
+        // memanggil data ke 5, berarti akan error
         cout << data.at(5) << endl;
     }
+    // kondisi jika error terjadi
 }
